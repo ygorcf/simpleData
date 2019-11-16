dff7a2d221b10ed2b8391b990766eb1ec31a1e17
Ygor Cruz <ygorcruzfelix@gmail.com>
2019-11-16 17:50:17 -0300

    Criado login simples

--- src/start.py
+++ src/start.py
@@ -1,27 +1,63 @@
 from tkinter import *
 
 class Application:
-  def __init__(self, master=None):
-    self.widget1 = Frame(master)
-    self.widget1.pack()
+  def __init__ (self, master=None):
+    self.font = ('Arial', '10')
 
-    self.msg = Label(self.widget1, text="Meu texto")
-    self.msg['font'] = ('Verdana', '10', 'italic', 'bold')
-    self.msg.pack()
+    self.container1 = Frame(master)
+    self.container1['pady'] = 10
+    self.container1.pack()
 
-    self.sair = Button(self.widget1)
-    self.sair['text'] = 'Sair'
-    self.sair['font'] = ('Calibri', '10')
-    self.sair['width'] = 5
-    self.sair.bind('<Button-1>', self.mudarTexto)
-    self.sair.pack()
+    self.container2 = Frame(master)
+    self.container2['padx'] = 20
+    self.container2.pack()
 
-  def mudarTexto(self, event):
-    print(event)
-    if self.msg['text'] == 'Meu texto':
-      self.msg['text'] = 'Meu widget clicado'
+    self.container3 = Frame(master)
+    self.container3['padx'] = 20
+    self.container3.pack()
+
+    self.container4 = Frame(master)
+    self.container4['pady'] = 20
+    self.container4.pack()
+
+    self.title = Label(self.container1, text='Dados do Usuário')
+    self.title['font'] = self.font + ('bold',)
+    self.title.pack()
+
+    self.nameLabel = Label(self.container2, text='Nome', font=self.font)
+    self.nameLabel.pack(side=LEFT)
+
+    self.name = Entry(self.container2)
+    self.name['width'] = 30
+    self.name['font'] = self.font
+    self.name.pack(side=LEFT)
+
+    self.passwordLabel = Label(self.container3, text='Senha', font=self.font)
+    self.passwordLabel.pack(side=LEFT)
+
+    self.password = Entry(self.container3)
+    self.password['width'] = 30
+    self.password['font'] = self.font
+    self.password['show'] = '*'
+    self.password.pack(side=LEFT)
+
+    self.button = Button(self.container4, text='Entrar')
+    self.button['font'] = ('Calibri', '14')
+    self.button['width'] = 18
+    self.button['command'] = self.verifyPassword
+    self.button.pack()
+
+    self.message = Label(self.container4, text='')
+    self.message.pack()
+
+  def verifyPassword (self):
+    name = self.name.get()
+    password = self.password.get()
+
+    if name == 'ygor' and password == 'senha':
+      self.message['text'] = 'Autenticado'
     else:
-      self.msg['text'] = 'Meu texto'
+      self.message['text'] = 'Usuario ou senha incorreta'
 
 root = Tk()
 Application(root)
